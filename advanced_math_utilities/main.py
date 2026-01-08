2f32492e8aa9270ace8c144c8412f99a59586e0f
Mr. Bentley <103165076+Tybent18@users.noreply.github.com>
2026-01-08 15:31:53 -0600

    Create main.py

---
+++ advanced_math_utilities/main.py
@@ -0,0 +1,7 @@
+from sequences import fibonacci
+from finance import compound_interest
+from physics import projectile_range
+
+print("Fibonacci(10):", fibonacci(10))
+print("Compound Interest:", compound_interest(1000, 0.05, 10))
+print("Projectile Range:", projectile_range(20, 45))
